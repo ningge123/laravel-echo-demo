1d8d08138310984c0e97f56fb0bcc4b97df6c416
cocoyo <2430114823@qq.com>
2018-05-18 15:12:20 +0800

    添加加入频道提醒

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,9 +10,11 @@ require('./bootstrap');
 window.Vue = require('vue');
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
 Vue.use(VueAxios, axios)
-
+Vue.use(iView);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -37,10 +39,22 @@ const app = new Vue({
         })
 
         Echo.join('chatroom')
+            .joining((user) => {// 方法会在其他新用户加入到频道时被执行
+                this.$Notice.success({
+                    title: '新用户加入提醒',
+                    desc: '用户:' + user.name + '加入房间'
+                });
+            })
             .listen('PushMessageEvent', (e) => {
                 e.message.user = e.user;
                 this.messages.push(e.message)
             })
+            .leaving((user) => { //会在其他用户退出频道时被执行
+                this.$Notice.error({
+                    title: '用户退出提醒',
+                    desc: '用户:' + user.name + '退出房间'
+                });
+            });
     },
     methods: {
         updateMessage(message) {
